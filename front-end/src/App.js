610deccfe846d5fe326f844927b9b822d0b59ac7
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-18 12:10:46 -0500

    installed react-router-dom. Added .env file and created NavBar page to test the frontend is working in App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,47 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-const API = process.env.REACT_APP_API_URL;
+import { BrowserRouter as Router } from "react-router-dom";
 
-console.log(API);
-function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+// PAGES
+import NavBar from "./pages/NavBar";
+
+export default function App() {
   return (
-    <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
+    <div className="App">
+      <Router>
+        <NavBar />
+        Welcome! We have many crafts items for sale.
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// const API = process.env.REACT_APP_API_URL;
+
+// console.log(API);
+// function App() {
+//   const [days, setDays] = useState([]);
+//   useEffect(() => {
+//     axios
+//       .get(`${API}/test`)
+//       .then(
+//         (response) => {
+//           setDays(response.data);
+//         },
+//         (error) => console.log("get", error)
+//       )
+//       .catch((c) => console.warn("catch", c));
+//   }, []);
+//   return (
+//     <div>
+//       <ul>
+//         {days.map((day) => (
+//           <li key={day.name}>{day.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ front-end/src/pages/NavBar.js
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <div>
+            <h1>Christina's Crafts Store</h1>
+        </div>
+    )
+}
