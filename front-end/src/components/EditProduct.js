ee9271e0b73ab439fccd15f79053522549e94c1d
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-19 20:32:37 -0500

    edit form is working. Edits any product from index page.

---
+++ front-end/src/components/EditProduct.js
@@ -0,0 +1,112 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+
+export default function EditProduct() {
+    let { id } = useParams();
+    const [product, setProduct] = useState({
+        name: "",
+        description: "",
+        price: 0,
+        rating: 0,
+        featured: null,
+        seller: "",
+        image: "",
+    });
+
+    const API = process.env.REACT_APP_API_URL;
+    let navigate = useNavigate();
+
+    const handleTextChange = (e) => {
+        setProduct({ ...product, [e.target.id]: e.target.value });
+    }
+
+    useEffect(() => {
+        axios.get(`${API}/products/${id}`)
+            .then((res) => {
+                setProduct(res.data);
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [id, navigate, API])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.put(`${API}/products/${id}`, product)
+            .then(() => {
+                navigate("/products")
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const handleCheckboxChange = (e) => {
+        setProduct({ ...product, featured: !product.featured });
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Product Name:</label>
+                <input 
+                    id="name"
+                    type="text" 
+                    value={product.name}
+                    onChange={handleTextChange}
+                    placeholder="Name..."
+                    required
+                />
+                <label htmlFor="description">Product Description:</label>
+                <input 
+                    id="description"
+                    type="text" 
+                    value={product.description}
+                    onChange={handleTextChange}
+                />
+                <label htmlFor="price">Price:</label>
+                <input 
+                    id="price"
+                    name="price"
+                    type="number"
+                    value={product.price}
+                    onChange={handleTextChange}
+                />
+                <label htmlFor="rating">Rating 0-5:</label>
+                <input 
+                    id="rating"
+                    type="number" 
+                    value={product.rating}
+                    onChange={handleTextChange}
+                />
+                <label htmlFor="featured">Featured:</label>
+                <input 
+                    id="featured"
+                    type="checkbox" 
+                    onChange={handleCheckboxChange}
+                    checked={product.featured}
+                />
+                <label htmlFor="seller">Seller:</label>
+                <input 
+                    id="seller"
+                    type="text" 
+                    value={product.seller}
+                    onChange={handleTextChange}
+                    placeholder="seller..."
+                />
+                <label htmlFor="image">image URL:</label>
+                <input 
+                    id="image"
+                    type="text"
+                    // pattern="http[s]*://.+"
+                    value={product.image}
+                    placeholder="http://"
+                    onChange={handleTextChange}
+                />
+                <input type="submit" />
+            </form>
+            <Link to={`/products/${id}`}>
+                <button>Nevermind!</button>
+            </Link>
+        </div>
+    )
+}
--- front-end/src/pages/Edit.js
+++ front-end/src/pages/Edit.js
@@ -1,9 +1,12 @@
+import EditProduct from "../components/EditProduct"
+
 export default function Edit() {
     return (
         <div>
             <h1>
                 Edit an existing product here!
             </h1>
+            <EditProduct />    
         </div>
         
     )
