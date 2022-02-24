d5a5eefe6e4d4a24a10020c060f76c4491d028b7
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-24 12:38:55 -0500

    added one item to seed data for testing deployment

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -5,7 +5,8 @@ INSERT INTO products (name, description, price, rating, featured, seller, image)
 ('Friendship Forever', 'Hearts friendship bracelet', 5, 2, false, 'Marisa', 'https://theneonteaparty.com/wp-content/uploads/2019/02/The-Neon-Tea-Party-Heart-Friendship-Bracelet-DIY-02.jpg'),
 ('Da Crofters Kep', 'Handmade knit hat Shetland wool', 40, 5, false, 'Xtina', 'https://images4-f.ravelrycache.com/uploads/JamiesonandSmith/779848613/d6a3577a-4314-4048-931f-0a682630d4a6_small2.jpg'),
 ('Felted Snowman', 'Needle-felted snowman for winter warmth', 85, 5, true, 'Elsa & Olaf', 'https://cdn.shopify.com/s/files/1/1278/3645/products/snowman-mini-needle-felting-kit-1_1024x1024.jpg?v=1629881723'),
-('Used Crayons', 'Much loved super jumbo crayons. Great for indoor wall murals!', 55, 2, true, 'friends', '/superJumbo.jpg'),
+('Used Crayons', 'Much loved super jumbo crayons. Great for indoor wall murals!', 55, 2, true, 'Lucy', '/superJumbo.jpg'),
+('Gecko Keychain', 'For gecko owners and reptile enthusiasts', 25, 2, false, 'Animal Zoo', 'https://i.etsystatic.com/17902850/r/il/bb3586/2564979969/il_fullxfull.2564979969_pez0.jpg')
 ('Ugly Knit Hat', 'Pattern and yarn recommended by yarn store owner', 3, 1, false, 'Crazy Yarn Lady', 'https://m.media-amazon.com/images/I/51CNCRmYneL._AC_UX385_.jpg'),
 ('Potholder', 'Handmade using a potholder loom', 65, 4, true, 'Elizabeth', 'https://images.squarespace-cdn.com/content/v1/53dff4d6e4b080a853debd62/1573332990600-CXX6GAUZL3LKFGV7KTDH/9ADE8EAC-14BE-47E5-B4EC-60B97C5BF098.JPG?format=2500w'),
 ('Button Necklace', 'If you like to be buttoned up, this necklace is for you!', 110, 4, false, 'Button Bandit', '/buttonNecklace.jpg'),
