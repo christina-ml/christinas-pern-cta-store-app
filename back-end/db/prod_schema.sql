b90a15b3cc1af7db22b527f564107c4866b11cb6
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-24 10:28:11 -0500

    heroku deployment again

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,8 +1,6 @@
--- DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS test;
 
--- CREATE TABLE test (
---     id SERIAL PRIMARY KEY, 
---     name TEXT
--- );
-
-PG_DATABASE=ddapmplp2i03ch
+CREATE TABLE test (
+    id SERIAL PRIMARY KEY, 
+    name TEXT
+);
--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,10 +1,8 @@
--- INSERT INTO test (name) VALUES
--- ('Monday'),
--- ('Tuesday'),
--- ('Wednesday'),
--- ('Thursday'),
--- ('Friday'),
--- ('Saturday'),
--- ('Sunday');
-
-PG_DATABASE=ddapmplp2i03ch
+INSERT INTO test (name) VALUES
+('Monday'),
+('Tuesday'),
+('Wednesday'),
+('Thursday'),
+('Friday'),
+('Saturday'),
+('Sunday');
