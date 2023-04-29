6352d79cd18af5a601c1afc3bcc6dcca1c7731b8
Christina Loiacono <loiaconochristina@gmail.com>
2023-04-29 17:58:43 -0400

    Add random word generator to FourOFour page, scss
    This gives the user a randomized word generator when they go to a page that doesnt exist.
    Add scss styling and hover effect, mouse pointers.

--- front-end/src/components/FourOFour/FourOFour.js
+++ front-end/src/components/FourOFour/FourOFour.js
@@ -1,28 +1,38 @@
-/*  
-    - Using the `map()` method to show a list of words beginning with the letter "C" from an array.
-*/
-const wordsWithC = `Cats, Crafts, Crayons, Crochet, Craftswoman, Craftsmanship, Collage, Crafting, Crazy-glue, Creative, Candle, Chair, Circles, Coffee, Cup, Clock, Clips, Cut, Create, Cookie Cutters, Clock, Candy, Card`;
+import { useState } from "react";
 
-let cWordArr = wordsWithC.split(", ");
-
-let listItems = cWordArr.map((word, index) => {
-  return <li key={index}>{word}</li>;
-});
+import './FourOFour.scss';
 
 export default function FourOFour() {
+  const [randomWord, setRandomWord] = useState('Crafts');
+  const wordsWithC = ['Cats', 'Crafts', 'Crayons', 'Crochet', 'Craftswoman', 'Craftsmanship', 'Collage', 'Crafting', 'Crazy-glue', 'Creative', 'Candle', 'Chair', 'Circles', 'Coffee', 'Cup', 'Clock', 'Clips', 'Cut', 'Create', 'Cookie cutters', 'Clock', 'Candy', 'Card'];
+  
+  // choose a random word based on random index
+  const handleRandomClick = () => {
+    let randomWordIndex = Math.floor(Math.random() * wordsWithC.length);
+    setRandomWord(wordsWithC[randomWordIndex]);
+  }
+
   return (
-    <div>
-      <h1>404 - Uh-oh! That page doesn't exist!</h1>
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1353/1353492.png"
-        alt="c is for cat"
-        width="150px"
-      />
-      <div>
-        C is for:
-        <ul>
-            {listItems}
-        </ul>
+    <div className="fourOFour">
+      <div className="fourOFour__errorMsg">
+        <h1>404 - Uh-oh! That page doesn't exist!</h1>
+      </div>
+      <div className="fourOFour__words">
+        <div className="fourOFour__words__cIsFor">
+          C is for...
+        </div>
+        <div>
+        <div className="fourOFour__words__wordsWithC" onClick={handleRandomClick}>
+          <div>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1353/1353492.png"
+              alt="c is for cat"
+              width="150px"
+            />
+          </div>
+          <div className="fourOFour__words__wordsWithC__randomWordEnding">{randomWord.slice(1)}</div>
+        </div>
+        </div>
       </div>
     </div>
   )
