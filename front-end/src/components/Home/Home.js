09594f2fc0cb327d4d10fdcc5b1a3a1ca593fb8c
Christina Loiacono <loiaconochristina@gmail.com>
2023-05-05 13:09:40 -0400

    Home.js, TopFive.js jsx & scss styling

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,13 +1,66 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 import "./Home.scss";
+import TopFive from "../TopFive/TopFive";
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function Home() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`${API}/products`)
+			.then((res) => {
+				setProducts(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
 	return (
 		<div className="home">
-			<h1>Welcome!</h1>
-			<p>
-				At Christina's Crafts Store, we have many crafts items for sale.
-			</p>
-			<p>Please take a look!</p>
+			<div className="home__banner">
+				<div className="home__banner__mainContent">
+					<div className="home__banner__mainContent__topFive">
+						<div className="home__banner__mainContent__topFive__topFiveHeader">
+							Top rated handcrafted treasures
+						</div>
+						<div className="home__banner__mainContent__topFive__topRatedProducts">
+							{/* show only top 5 rated products */}
+							{products
+								.sort((a, b) => (a.rating < b.rating ? 1 : -1))
+								.slice(0, 5)
+								.map((product, index) => {
+									const src =
+										product.image.substring(0, 8) ===
+										"https://"
+											? product.image
+											: API + product.image;
+
+									return (
+										<TopFive
+											product={product}
+											src={src}
+											id={index}
+											key={product.id + index}
+										/>
+									);
+								})}
+						</div>
+					</div>
+				</div>
+				<div className="home__banner__welcome">
+					<h1>Welcome!</h1>
+					<p>
+						At Christina's Crafts Store, we have many crafts items
+						for sale.
+					</p>
+					<p>Please take a look!</p>
+				</div>
+			</div>
 		</div>
 	);
 }
