4aea383062407c31824762c95ffff78066bd4082
Christina Loiacono <loiaconochristina@gmail.com>
2023-05-05 13:09:40 -0400

    Home.js, TopFive.js jsx & scss styling for Home page

---
+++ front-end/src/components/TopFive/TopFive.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import setStars from "../../helpers/setStars";
+
+import './TopFive.scss';
+
+const TopFive = ({key, product, src, id}) => {
+	return (
+		<div className="topFiveProducts">
+			<Link to={`/products/${product.id}`}>
+				<div className="topFiveProducts__image">
+					<img src={src} alt={product.name} />
+				</div>
+				<div className="topFiveProducts__productName">{product.name}</div>
+				<div>{setStars(product)}</div>
+			</Link>
+		</div>
+	);
+};
+
+export default TopFive;
